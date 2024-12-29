806807a8ae8bf570e726e1f5b48efa9b09e68eb4
Nithin <nithindevathi@gmail.com>
2024-12-29 17:37:20 +0530

    feat:users able to see their blogs

--- src/components/PostListItem.jsx
+++ src/components/PostListItem.jsx
@@ -1,31 +1,29 @@
 import { Link } from "react-router-dom";
+import { format } from "timeago.js";
 
-function PostListItem() {
+function PostListItem({ post }) {
+  console.log(post);
   return (
     <div>
       <div className="flex flex-col xl:flex-row gap-4 my-5">
-        <img
-          src="/postImg.jpeg"
-          className="xl:w-1/3 rounded-3xl object-fill aspect-video md:hidden xl:block "
-        />
+        {post.img && (
+          <img
+            src={post.img}
+            className="xl:w-1/3 rounded-3xl object-fill aspect-video md:hidden xl:block "
+          />
+        )}
         <div className="flex flex-col justify-around">
-          <h1 className="text-2xl font-semibold">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-            nesciunt ipsam.
-          </h1>
+          <h1 className="text-2xl font-semibold">{post.title}</h1>
           <div className="space-x-2">
             <span className="text-sm">Wriiten by</span>
-            <span className="text-blue-500 text-sm">John Doe</span>
-            <span className="text-sm">Web Design</span>
-            <span className="text-sm">2 days ago</span>
+            <span className="text-blue-500 text-sm">{post.user.email}</span>
+            <span className="text-sm">{post.category}</span>
+            <span className="text-sm">{format(post.createdAt)}</span>
           </div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut,
-            corrupti placeat quae ad error esse totam vitae consequuntur
-            nesciunt repellat aperiam dolores! Aut voluptatibus numquam dicta
-            consectetur iusto, est optio.
-          </p>
-          <Link className="text-blue-600">Read More</Link>
+          <p>{post.content.substring(0, 250)}...</p>
+          <Link className="text-blue-600" to={`/${post.slug}`}>
+            Read More
+          </Link>
         </div>
       </div>
     </div>
---
+++ src/components/Sidemenu.jsx
@@ -0,0 +1,5 @@
+function Sidemenu() {
+  return <div>Sidemenu</div>;
+}
+
+export default Sidemenu;
