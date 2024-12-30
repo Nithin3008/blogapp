b90654b7d99805cded40f1143b2dfe96f8160ed1
Nithin <nithindevathi@gmail.com>
2024-12-30 11:09:48 +0530

    minor changes

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import PostLists from "../components/PostLists";
+import Sidemenu from "../components/Sidemenu";
 
 export default function PostList() {
-  return <div>PostList</div>;
+  const [openFilter, setFilter] = useState(false);
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+      <div className="flex">
+        <div>
+          <PostLists />
+        </div>
+        <div>
+          <button
+            className="visible md:hidden"
+            onClick={() => setFilter((filter) => !filter)}
+          >
+            Filter or Search
+          </button>
+          <div className={`${openFilter ? "visible" : "hidden"} md:block`}>
+            <Sidemenu />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
